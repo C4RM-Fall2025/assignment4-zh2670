f3986c0277e1858c4a6fa357a6aa222cf78ca4d6
zh2670 <zh2670@columbia.edu>
2025-10-06 00:14:01 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,10 +1,11 @@
-def get_bond_price_E(face,couponRate,m,yc):
-  pvcfsum=0
-  for t in range(1,m+1):
-    if t==m:
-      coupon=face*couponRate+face
-    else:
-      coupon=face*couponRate
-    pvcf=coupon*(1+yc[t-1])**(-t)
-    pvcfsum +=pvcf
-  return pvcfsum
+def getBondPrice_E(face, couponRate, yc):
+    m = len(yc) 
+    bondPrice = 0
+    
+    for t, y in enumerate(yc, start=1):
+        C = face * couponRate if t < m else face * (1 + couponRate)
+        pv = 1 / ((1 + y) ** t) 
+        pvcf = C * pv 
+        bondPrice += pvcf 
+    
+    return bondPrice
